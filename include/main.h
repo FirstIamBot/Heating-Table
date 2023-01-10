5607606ee785fb43bd40d19e96506e0fb54d69a0
Henn Kuz <kgena0671@gmail.com>
2023-01-10 23:29:55 +0200

    1

--- include/main.h
+++ include/main.h
@@ -1,33 +1,55 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+//*************************  MODE and State working HEAT_TABLE ******************************
+// MODE FLAG's
+#define STANDBAY 0
+#define MANUAL_HEATING 1
+#define PROG_HEATING   2
+#define SETTING  4
+#define HEATING 8
+#define CUR_MES 16
+// State FLAG's
+#define CUR_MES_ON  1 // вкл Вывода выбранной(ручной) температуры 
+#define CUR_MES_OFF 0 // выкл Вывода выбранной(ручной) температуры
+#define HEATING_ON  1
+#define HEATING_OFF 0
+// State FLAG's Button Encoder
+#define NOT_PRESS   0
+#define SHORT_PRESS 1
+#define LONG_PRESS  2
+// Time key pres mSec
+#define SHORT_PRESS_TIME 500
+#define LONG_PRESS_TIME  3000
 //************************************** I2c Bus  ***********************************
 #define SCL 4
 #define SDA 5
 //************************************** LCD  ***************************************
 #define SCREEN_WIDTH  128
 #define SCREEN_HEIGHT 64
+#define GUI_UPDATE_DELAY 500
+#define GUI_TIME_DELAY 3000 // вывод установленой ручной температуры 3 сек
 //*******************************      SPI Bus for MAX6675     **********************
+#define TEMP_READ_DELAY 1000
 int MAX6675_DO  = 14; // голубой
 int MAX6675_CS  = 12; // фиолетовый
 int MAX6675_CLK = 13; // серый
 //********************************     Rotary Encoder    ******************************
-#define ROTARY_ENCODER_A_PIN      15 // CLK(А)     синий
-#define ROTARY_ENCODER_B_PIN      3  // DT(В)      зелёный 
-#define ROTARY_ENCODER_BUTTON_PIN 1  // SW(Button) тёмно серый
+#define ROTARY_ENCODER_A_PIN      3  // CLK(А)     синий   25 
+#define ROTARY_ENCODER_B_PIN      1  // DT(В)      зелёный 26
+#define ROTARY_ENCODER_BUTTON_PIN 15  // SW(Button) фиолетовый
 #define ROTARY_ENCODER_VCC_PIN   -1   //
 //depending on your encoder - try 1,2 or 4 to get expected behaviour
 //#define ROTARY_ENCODER_STEPS 1
 //#define ROTARY_ENCODER_STEPS 2
 #define ROTARY_ENCODER_STEPS 4
 
-#define ShortPress 0;
-#define LongPress  1;
 // ********************************** RBD dimmer ************************************
 #define OUTPUT_PIN 4 //12 
 #define ZEROCROSS  2 //7 // 5 for boards with CHANGEBLE input pins
 
 #define TEMPHEATMAXVALUE 100
+// ************************ PID controller  settings and gains **********************
+double Kp=2, Ki=5, Kd=1;
 // ************************************************************************************* 
 
 #endif // end 
--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,44 @@
 #include "max6675.h"
 #include <AiEsp32RotaryEncoder.h>
 #include <RBDdimmer.h>
+#include <PID_v1.h>
 //#include < AiEsp32RotaryEncoderNumberSelector.h>
 #include "image.h"
 #include "main.h"
 
 //********************* User variables ******************************
-float temp;
-float Curr_temp;
-float Measured_temp;
-
+//float Temperature, Curr_Temp, Measured_Temp;
 int64_t Encpos;
 int8_t Button;
+//***********  mode reg (Режим роботы нагревательного стола ) **************************
+//    7     6      5       4       3       2              1          0
+// |     |     |CUR_MES|HEATING|SETTING|PROG_HEATING|MANUAL_HEATING|STANDBAY|
+//
+//********************************************************************
+int8_t Mode; //
+//***********  status mode reg (Статус нагревательного стола ) ****************
+//    7     6     5     4     3     2     1      0
+// |     |     |     |     |     |     |      |     |
+//
+//********************************************************************
+int8_t State;//
 
 int8_t Current_pos;
 int8_t Select_pos;
 
+double Temperature, Measured_Temp, Curr_Temp, SetPoint, OutputVal;
 //********************************************************************
 void DislayLogo(void);
-float temp_loop(void);
+bool temp_loop(void);
 void rotary_loop(void);
 void rotary_onButtonClick(void);
+uint8_t rotary_EncoderButton(void);
+
 void DisplayTempEncoder(int16_t x, int16_t y, int32_t EncPosition, float t);
-
-
+void DisplayTemp(int16_t x, int16_t y, double *temp);
+bool temp_loop_pntr(double *temperature);
+bool loop_GUI(double *temperature, uint32_t epcos);
+void controler_loop(void);
 //********************************************************************
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(
@@ -42,11 +57,19 @@ AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(
                     ROTARY_ENCODER_VCC_PIN, \
                     ROTARY_ENCODER_STEPS);
 MAX6675 thermocouple(MAX6675_CLK, MAX6675_CS, MAX6675_DO);
-dimmerLamp tableHeat(OUTPUT_PIN, ZEROCROSS); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
+dimmerLamp TableHeat(OUTPUT_PIN, ZEROCROSS); //initialase port for dimmer for ESP8266, ESP32, Arduino due boards
+
+PID TableHeatPID(&Measured_Temp, &OutputVal, &SetPoint, Kp, Ki, Kd, DIRECT);
+//***********************************************************************
+void IRAM_ATTR readEncoderISR()
+{
+  rotaryEncoder.readEncoder_ISR();
+}
 //***********************************************************************
 void setup() {
   Serial.begin(115200);
   // Start I2C Communication SDA = 5 and SCL = 4 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
+  //*****************************    SSD1306 OLED    ********************
   Wire.begin(SDA, SCL);
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false, false)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -60,36 +83,145 @@ void setup() {
   delay(200);
   //****************************    rotaryEncoder    *********************
   rotaryEncoder.begin();
-  rotaryEncoder.setup([]{rotaryEncoder.readEncoder_ISR();}); // установка прерываний для Энкодера
+  rotaryEncoder.setup(readEncoderISR);
+  //rotaryEncoder.setup([]{rotaryEncoder.readEncoder_ISR();}); // установка прерываний для Энкодера
   //optionally we can set boundaries and if values should cycle or not
   bool circleValues = false;
-  rotaryEncoder.setBoundaries((int)thermocouple.readCelsius(), 400, circleValues);// Установка границы от текущей темрературы до максимальнй
-  //rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
+  rotaryEncoder.setBoundaries(0, 400, circleValues);// Установка границы от текущей темрературы до максимальной
   rotaryEncoder.setAcceleration(150); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
-  Serial.println("rotaryEncoder init");
+  //rotaryEncoder.disableAcceleration(); //acceleration is now enabled by default - disable if you dont need it
+  rotaryEncoder.setEncoderValue(int(thermocouple.readCelsius())); // init start value rotary encoder
+  Serial.println("RotaryEncoder init");
+  // ***************************    PID regulator    **************************
+  //set point 100% or 400 Celsium
+  SetPoint = 100;
+  //set PID update interval to 1000ms
+  TableHeatPID.SetSampleTime(1000);
+  Serial.println("PID regulator init");
   // ***************************    RBD dimmer    **************************
-  tableHeat.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE) 
-  tableHeat.setPower(0);
+  TableHeat.begin(NORMAL_MODE, ON); //dimmer initialisation: name.begin(MODE, STATE) 
+  TableHeat.setPower(0);
   Serial.println("RBD dimmer init");
+  //***************************************
+  State=0;
 }
 //***********************************************************************
 void loop() {
-  // put your main code here, to run repeatedly:
+  static char outstr[6];
+
+  temp_loop_pntr(&Measured_Temp);
+
+  controler_loop();
+  if(Mode&CUR_MES){
+    //Serial.print(" Curr_Temp=");
+    //Serial.println(String(Curr_Temp, DEC));
+    Temperature = Curr_Temp;
+  }
+  else{
+    Temperature = Measured_Temp;
+  }
+  
+  //if(State&HEATING_ON){ //
+    TableHeatPID.Compute();
+    State |= HEATING_ON;
+    
+    Serial.print(" OutputVal=");
+    Serial.println(dtostrf(OutputVal ,4, 2, outstr));
+    TableHeat.setPower(int(OutputVal));
+    
+
+  //}    
+
+  loop_GUI(&Temperature, Encpos);
+
+
 }
-
-
 //########################################################################################
-void DislayLogo(void){
-        display.clearDisplay();
-        display.setFont(&FreeSerif12pt7b);
-        display.setTextSize(1);
-        display.setTextColor(WHITE);
-        display.setCursor(20, 20);
-        display.print("Heating");
-        display.setCursor(30, 40);
-        display.print("Table");
-        display.display();
-        delay(3000);
+void controler_loop(void){
+  uint8_t button; 
+  static unsigned long lastTimePressed = 0;
+  static unsigned long lastTimeWait = 0;
+  unsigned long TimePressed;
+  static unsigned long lastTimeRotary = 0; // переменная для формированния задержки перед сохранением
+  //*****************************************
+  // Encoder rotary
+  //*****************************************
+  //dont print anything unless value changed
+	if (rotaryEncoder.encoderChanged())
+	{
+    if(Mode & STANDBAY){
+      State |= HEATING_OFF;
+      return;
+    }
+    if(Mode & MANUAL_HEATING){
+      Mode |= CUR_MES;         // Установка флага вывода выбранной(ручной) температуры 
+      State |= HEATING_OFF;
+      Curr_Temp = rotaryEncoder.readEncoder();
+      //Serial.print(" State=");
+      //Serial.println(String(State, BIN));
+      //Serial.print(" Mode=");
+      //Serial.println(String(Mode, BIN));
+    }
+    if(Mode & PROG_HEATING){
+      Current_pos = rotaryEncoder.readEncoder();
+    }
+    if(Mode & SETTING){
+      Current_pos = rotaryEncoder.encoderChanged();
+      if (Current_pos>0) Serial.print("+");
+      if (Current_pos<0) Serial.print("-"); 
+    }
+	}
+  //*****************************************
+  //  Button click
+  //*****************************************
+  button = rotary_EncoderButton();
+  if(button){
+    //Serial.print(" button=");
+    //Serial.println(String(button, BIN));
+    //Serial.print(" Mode=");
+    //Serial.println(String(Mode, BIN));
+    if(button & SHORT_PRESS){
+       //Serial.println("A SHORT press is detected in controler_loop");
+      if(Mode == STANDBAY){
+        Mode &= ~STANDBAY;
+        Mode |= MANUAL_HEATING;
+        Serial.println("Mode == STANDBAY");
+      }
+      else if(Mode & MANUAL_HEATING){
+        Mode &= ~MANUAL_HEATING;
+        Mode |= PROG_HEATING;
+        Serial.println("Mode == MANUAL_HEATING");
+      }
+      else if(Mode & PROG_HEATING){
+        Mode &= ~PROG_HEATING;
+        Mode |= SETTING;
+        Serial.println("Mode == PROG_HEATING");
+      }
+      else if(Mode & SETTING){
+        Mode &= ~SETTING;
+        Mode |= STANDBAY;
+        Serial.println("Mode == SETTING");
+      }
+    }
+    if(button & LONG_PRESS){
+        Serial.println("A detected is LONG press in controler_loop");
+    }
+  }
+  // myflags &= ~option4; // выключаем option4
+  // myflags ^= option4; // включаем или выключаем option4 
+  // if (myflags & option4) ... // если option4 установлено - что-нибудь делаем
+  // myflags |= option4; // включаем option4
+  //  if ((mask & value != 0U)
+  //  {
+  //      value &= ~myflags; clear
+  //  }
+  //***************************************************************************
+  if((Mode&CUR_MES) && (millis()-lastTimeWait > GUI_TIME_DELAY)){
+    Mode &= ~CUR_MES; //   Сброс бита для вывода устанавливаемой температуры через 3 сек 
+    lastTimeWait = millis();
+    State |= HEATING_ON;
+  }
+  
 }
 
 void rotary_loop(void)
@@ -97,7 +229,16 @@ void rotary_loop(void)
 	//dont print anything unless value changed
 	if (rotaryEncoder.encoderChanged())
 	{
-    Encpos = rotaryEncoder.readEncoder();
+    if(State == 0){
+      return;
+    }
+    if(State == 1){
+      Encpos = rotaryEncoder.readEncoder();
+      Curr_Temp = Encpos;
+    }
+    if(State == 2){
+      Current_pos = rotaryEncoder.readEncoder();
+    }
 	}
 	if (rotaryEncoder.isEncoderButtonClicked())
 	{
@@ -113,25 +254,78 @@ void rotary_onButtonClick(void)
 	{
 		return;
 	}
+
+		Button = SHORT_PRESS;
+    switch (State)
+    {
+    case STANDBAY:/* constant-expression */
+      State |= HEATING;
+      break;
+    case HEATING:/* constant-expression */
+      State |= MANUAL_HEATING;
+      break;
+    case MANUAL_HEATING:/* constant-expression */
+      State = PROG_HEATING;
+      break;
+    case PROG_HEATING:/* constant-expression */
+      State = SETTING;
+      break;
+    case SETTING:/* constant-expression */
+      State = STANDBAY;
+      break;     
+    default:
+      break;
+    }
 	lastTimePressed = millis();
 }
 
-float temp_loop(void){
+bool temp_loop(void){
   static float lastTemp;
-	static unsigned long lastTimeTemp = 0;
+	static unsigned long lastTempUpdate = 0;
   
 	// Плавное отображение(вывод) изменения температуры
-	if (millis() - lastTimeTemp < 150)
+	if (millis() - lastTempUpdate > TEMP_READ_DELAY)
 	{
-		return lastTemp;
+    Measured_Temp = thermocouple.readCelsius();
+    lastTempUpdate = millis();
+		return true;
 	}
-	lastTimeTemp = millis();
-  temp = thermocouple.readCelsius();
-  lastTemp = temp;
-  return temp;
+  return false;
 }
 
-void DisplayTempEncoder(int16_t x, int16_t y, int32_t EncPosition, float t){
+bool temp_loop_pntr(double *temperature){
+  static float lastTemp;
+	static unsigned long lastTempUpdate = 0;
+  
+	// Переодическое измерение температуры
+	if (millis() - lastTempUpdate > TEMP_READ_DELAY)
+	{
+    *temperature = double(thermocouple.readCelsius());
+    lastTempUpdate = millis();
+		return true;
+	}
+  return false;
+}
+ 
+void DisplayTemp(int16_t x, int16_t y, double *temp){
+  static char outstr[6];
+  static unsigned long lastLCDUpdate;
+
+	if (millis() - lastLCDUpdate > 500)
+	{
+    //display.clearDisplay();
+    display.setFont(&FreeSerif12pt7b);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(x, y);
+    display.print(dtostrf(*temp ,4, 2, outstr));
+    //display.print(" C");
+    display.display();
+    lastLCDUpdate = millis();
+  }
+}
+
+void DisplayTempEncoder(int16_t x, int16_t y, int32_t EncPosition, float Tmp){
   static char outstr[6];
 
   display.clearDisplay();
@@ -140,16 +334,114 @@ void DisplayTempEncoder(int16_t x, int16_t y, int32_t EncPosition, float t){
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(x, y);
-  display.print("Pos = ");
+  display.print("Pos=");
   display.print(String((int32_t)EncPosition));
-  Serial.print("Pos = ");
-  Serial.print(String((int32_t)EncPosition));
-  //***********************************************************
+  display.print("S=");
+  display.print(String((int8_t)State));
+  //*********************************************************
   display.setFont(&FreeSerif12pt7b);
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(x+30, y+30);
-  display.print(dtostrf(t,4, 2, outstr));
+  display.print(dtostrf(Tmp,4, 2, outstr));
   display.print(" C '");
+  //display.display();
+    //*********************************************************
+  display.setFont(&FreeMono9pt7b);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(x+50, y+20);
+  display.print(dtostrf(Tmp,4, 2, outstr));
+  display.print("B=");
+  display.print(String((int8_t)Button));
   display.display();
  }
+
+void DislayLogo(void){
+        display.clearDisplay();
+        display.setFont(&FreeSerif12pt7b);
+        display.setTextSize(1);
+        display.setTextColor(WHITE);
+        display.setCursor(20, 20);
+        display.print("Heating");
+        display.setCursor(30, 40);
+        display.print("Table");
+        display.display();
+        delay(3000);
+}
+
+bool loop_GUI(double *temperature, uint32_t epcos){
+
+	static unsigned long lastGUIUpdate = 0;
+  static char outstr[6];
+
+	// Плавное отображение(вывод) изменения температуры
+	if (millis() - lastGUIUpdate > GUI_UPDATE_DELAY)
+	{
+    display.clearDisplay();
+    if(Mode == STANDBAY){
+      display.setFont(&FreeSerif9pt7b);
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(25, 55);
+      display.print("Standbay");
+      display.setFont(&FreeSerif9pt7b);
+
+      DisplayTemp(25, 25, temperature);  
+    }
+    if(Mode & MANUAL_HEATING){
+      display.setFont(&FreeSerif9pt7b);
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      if(Mode&CUR_MES){  // Выбор стрелок или нагревания 
+        display.drawBitmap(5, 20, epd_bitmap_up, 20, 36, WHITE); // заменить 36 на 40
+        display.drawBitmap(110, 20, epd_bitmap_down, 20, 36, WHITE);        
+      }
+      else if(State&HEATING_ON){
+        display.drawBitmap(5, 20, epd_bitmap_heating_table, 22, 21, WHITE);// подобрать 22,21
+      }
+      DisplayTemp(30, 40, temperature); 
+    }
+    if(Mode & PROG_HEATING){
+      display.setFont(&FreeSerif9pt7b);
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(25, 15);
+      display.print("Prog_heating");
+
+      DisplayTemp(25, 55, temperature); 
+    }
+    if(Mode & SETTING){
+      display.setFont(&FreeSerif9pt7b);
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(20, 20);
+      display.print("Setting");
+      display.display();
+      //DisplayTemp(10, 10, temperature); 
+    }
+    lastGUIUpdate = millis();
+		return true;
+	}
+  return false;
+ }
+
+uint8_t rotary_EncoderButton(void){
+
+    unsigned long  pressDuration;
+
+    pressDuration = rotaryEncoder.isEncoderButtonType();
+
+   //if( pressDuration < SHORT_PRESS_TIME ){
+     // return NOT_PRESS;
+   //}
+   if( (SHORT_PRESS_TIME < pressDuration) && (LONG_PRESS_TIME > pressDuration ) ){
+      //Serial.println("A SHORT press is detected");
+      return SHORT_PRESS;
+    }
+    if( LONG_PRESS_TIME < pressDuration){
+      //Serial.println("A detected is LONG press ");
+      return LONG_PRESS;      
+    }
+    return NOT_PRESS;
+}
